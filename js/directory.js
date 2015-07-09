da55dd64e14b36ba8cae8f8690469a5c26d87226
Rand Seay <rand.seay@gmail.com>
2015-07-09 01:13:41 -0500

    Finish input navigation.

---
+++ js/directory.js
@@ -0,0 +1,103 @@
+var DIRECTORY = [
+    {
+        'name': 'about',
+        'url': '/about',
+        'type': 'internal',
+        'keywords': [
+            'about',
+            'learn',
+            'learn about',
+            'bio'
+        ]
+    },
+    {
+        'name': 'blog',
+        'url': '/articles',
+        'type': 'internal',
+        'keywords': [
+            'articles',
+            'blog',
+            'read',
+            'read articles'
+        ]
+    },
+    {
+        'name': 'contact',
+        'url': '/contact',
+        'type': 'internal',
+        'keywords': [
+            'contact',
+            'get in touch'
+        ]
+    },
+    {
+        'name': 'email',
+        'url': 'mailto:rand@skosh.io',
+        'type': 'internal',
+        'keywords': [
+            'mail',
+            'email'
+        ]
+    },
+    {
+        'name': 'fuselage',
+        'url': 'http://fuselage.skosh.io',
+        'type': 'external',
+        'keywords': [
+            'fuselage',
+            'framework'
+        ]
+    },
+    {
+        'name': 'github',
+        'url': 'http://github.com/randseay',
+        'type': 'external',
+        'keywords': [
+            'code',
+            'github'
+        ]
+    },
+    {
+        'name': 'home',
+        'url': '/',
+        'type': 'internal',
+        'keywords': [
+            'go home',
+            'home',
+            'index'
+        ]
+    },
+    {
+        'name': 'skosh',
+        'url': 'http://www.skosh.io',
+        'type': 'external',
+        'keywords': [
+            'a little different',
+            'skosh',
+            'teamskosh'
+        ]
+    },
+    {
+        'name': 'twitter',
+        'url': 'http://twitter.com/randseay',
+        'type': 'external',
+        'keywords': [
+            'tweet',
+            'twitter'
+        ]
+    },
+    {
+        'name': 'work',
+        'url': '/work',
+        'type': 'internal',
+        'keywords': [
+            'my work',
+            'portfolio',
+            'view',
+            'view work',
+            'view my work',
+            'view your work',
+            'work'
+        ]
+    }
+];
